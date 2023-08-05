e24cbd1a5c1b8e8a91b1612f5f5a79ee5079a17d
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-08-05 18:54:44 -0300

    Folder update

---
+++ lib/pages/search_page/search_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zip_search/data/cubits/search_zip/search_zip_cubit.dart';
+import 'package:zip_search/data/cubits/search_zip/search_zip_state.dart';
+import 'package:zip_search/pages/search_page/widgets/initial_widget.dart';
+import 'package:zip_search/pages/search_page/widgets/success_widget.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _HomeState();
+}
+
+class _HomeState extends State<SearchPage> {
+  SearchZipCubit get searchZipCubit => context.read<SearchZipCubit>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: BlocConsumer<SearchZipCubit, SearchZipState>(
+          bloc: searchZipCubit,
+          listener: listener,
+          builder: builder,
+        ),
+      );
+
+  void listener(BuildContext context, SearchZipState state) {
+    if (state is ErrorSearchZipState) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(state.errorMessage),
+          duration: const Duration(seconds: 5),
+        ),
+      );
+    }
+  }
+
+  Widget builder(BuildContext context, SearchZipState state) {
+    if (state is LoadingSearchZipState) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is FetchedSearchZipState) {
+      return SuccessWidget(
+        address: state.address,
+      );
+    } else {
+      return const InitialWidget();
+    }
+  }
+}
