b4399fc649455618731a82bb00ffb2eb6127e87b
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-08-05 18:17:50 -0300

    Added new home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zip_search/data/cubits/navigation/navigation_cubit.dart';
 import 'package:zip_search/data/cubits/search_zip/search_zip_cubit.dart';
 import 'package:zip_search/data/via_cep_repository.dart';
-import 'package:zip_search/pages/home_page/home_page.dart';
+import 'package:zip_search/pages/root_page/root_page.dart';
 
 void main() {
   runApp(MainApp());
@@ -16,14 +17,18 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       home: MultiBlocProvider(
         providers: [
           BlocProvider(
             create: (context) => SearchZipCubit(),
           ),
+          BlocProvider(
+            create: (context) => NavigationCubit(),
+          ),
         ],
-        child: const HomePage(),
+        child: const RootPage(),
       ),
     );
   }
