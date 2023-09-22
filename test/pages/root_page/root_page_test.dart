1bca733942a5103b5514687a18b1a702524f6542
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-09-22 16:53:35 -0300

    Removed unnecessary import

--- test/pages/root_page/root_page_test.dart
+++ test/pages/root_page/root_page_test.dart
@@ -2,7 +2,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:zip_search/data/cubits/favorites/favorites_cubit.dart';
 import 'package:zip_search/data/cubits/navigation/navigation_cubit.dart';
 import 'package:zip_search/data/cubits/navigation/navigation_state.dart';
