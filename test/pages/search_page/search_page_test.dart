4da3ba76b0a81fccce875d8d7a068f086e93f740
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-09-25 18:06:22 -0300

    Wrote new tests

--- test/pages/search_page/search_page_test.dart
+++ test/pages/search_page/search_page_test.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:zip_search/commons/app_strings.dart';
 import 'package:zip_search/data/cubits/search_zip/search_zip_cubit.dart';
 import 'package:zip_search/data/cubits/search_zip/search_zip_state.dart';
 import 'package:zip_search/model/address_model.dart';
@@ -54,7 +57,129 @@ void main() {
 
     await _createWidget(tester);
 
+    expect(find.byKey(InitialWidget.initialWidgetKey), findsOneWidget);
+
     expect(find.byType(SuccessWidget), findsOneWidget);
+  });
+
+  group('Flushbar messages |', () {
+    group('Success |', () {
+      testWidgets('Find success flushbar after search valid ZIP',
+          (tester) async {
+        await tester.runAsync(() async {
+          final state = StreamController<SearchZipState>();
+
+          whenListen(
+            searchZipCubit,
+            state.stream,
+            initialState: InitialSearchZipState(),
+          );
+
+          await _createWidget(tester);
+
+          state.add(
+            FavoritedAddressZipState(
+              message: AppStrings.successZipFavoriteText,
+            ),
+          );
+          await tester.pump();
+          await tester.pump();
+          await tester.pump();
+
+          expect(
+            find.text(AppStrings.successZipFavoriteText),
+            findsOneWidget,
+          );
+        });
+      });
+    });
+
+    group('Errors |', () {
+      testWidgets('Find flushbar after search empty error', (tester) async {
+        await tester.runAsync(() async {
+          final state = StreamController<SearchZipState>();
+          whenListen(
+            searchZipCubit,
+            state.stream,
+            initialState: InitialSearchZipState(),
+          );
+
+          await _createWidget(tester);
+
+          state.add(
+            ErrorEmptyZipState(
+              errorEmptyMessage: AppStrings.zipCodeEmptyErrorMessageText,
+            ),
+          );
+
+          await tester.pump();
+          await tester.pump();
+          await tester.pump();
+
+          expect(
+            find.text(AppStrings.zipCodeEmptyErrorMessageText),
+            findsOneWidget,
+          );
+        });
+      });
+
+      testWidgets('Find flushbar after search an invalid zip', (tester) async {
+        await tester.runAsync(() async {
+          final state = StreamController<SearchZipState>();
+
+          whenListen(
+            searchZipCubit,
+            state.stream,
+            initialState: InitialSearchZipState(),
+          );
+
+          await _createWidget(tester);
+
+          state.add(
+            ErrorSearchZipState(
+              errorMessage: AppStrings.zipCodeInvalidErrorMessageText,
+            ),
+          );
+
+          await tester.pump();
+          await tester.pump();
+          await tester.pump();
+
+          expect(
+            find.text(AppStrings.zipCodeInvalidErrorMessageText),
+            findsOneWidget,
+          );
+        });
+      });
+
+      testWidgets('Find flushbar after add an already favorited address',
+          (tester) async {
+        await tester.runAsync(() async {
+          final state = StreamController<SearchZipState>();
+
+          whenListen(
+            searchZipCubit,
+            state.stream,
+            initialState: InitialSearchZipState(),
+          );
+
+          await _createWidget(tester);
+
+          state.add(
+            ErrorAlreadyAddedZipState(
+              errorMessage: AppStrings.alreadyFavoritedZipCodeText,
+            ),
+          );
+
+          await tester.pump();
+          await tester.pump();
+          await tester.pump();
+
+          expect(find.text(AppStrings.alreadyFavoritedZipCodeText),
+              findsOneWidget);
+        });
+      });
+    });
   });
 }
 
