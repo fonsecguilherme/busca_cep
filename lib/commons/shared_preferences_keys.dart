d09f292811eb106f28a41249d59d75cbc2c7625a
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-10-12 22:22:24 -0300

    Added new shared preferences keys

--- lib/commons/shared_preferences_keys.dart
+++ lib/commons/shared_preferences_keys.dart
@@ -1,3 +1,6 @@
 class SharedPreferencesKeys {
   static String boolKey = 'isFirstExecution';
+  static String counterSearchedZipsKeys = 'counterSearchedZipsKeys';
+  static String counterFavZips = 'counterFavZips';
+  static String savedZipKey = 'savedZipKey';
 }
