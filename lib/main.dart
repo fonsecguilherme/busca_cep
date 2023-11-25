35f24bdb6343261f26d8fd26826ecc67daa4f567
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-11-24 23:29:35 -0300

    Separated widget from mais class

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zip_search/app.dart';
 import 'package:zip_search/commons/shared_preferences_keys.dart';
-import 'package:zip_search/pages/root_page/root_page.dart';
-import 'package:zip_search/pages/welcome_page/welcome_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,38 +9,10 @@ void main() async {
   var prefs = await SharedPreferences.getInstance();
   var isFirstExecution = prefs.getBool(SharedPreferencesKeys.boolKey) ?? true;
 
-  runApp(MainApp(
-    prefs: prefs,
-    isFirstExecution: isFirstExecution,
-  ));
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({
-    super.key,
-    required this.prefs,
-    required this.isFirstExecution,
-  });
-
-  final SharedPreferences prefs;
-
-  final bool isFirstExecution;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple,
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: isFirstExecution
-          ? WelcomePage(
-              prefs: prefs,
-            )
-          : const RootPage(),
-    );
-  }
+  runApp(
+    MyApp(
+      prefs: prefs,
+      isFirstExecution: isFirstExecution,
+    ),
+  );
 }
