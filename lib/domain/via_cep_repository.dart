ba76073bdc8df8e43ee39d0ae192191b52c7eb6f
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-09-04 21:02:35 -0300

    feat: use new custom exceptions

--- lib/domain/via_cep_repository.dart
+++ lib/domain/via_cep_repository.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:zip_search/core/model/address_model.dart';
 
+import '../core/exceptions/custom_exceptions.dart';
+
 abstract interface class IViaCepRepository {
   Future<AddressModel?> fetchAddress(String zipCode);
 }
@@ -11,18 +13,21 @@ class ViaCepRepository implements IViaCepRepository {
   @override
   Future<AddressModel?> fetchAddress(String zipCode) async {
     final String url = 'https://viacep.com.br/ws/$zipCode/json/';
-
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final address = jsonDecode(response.body);
+
       if (address['erro'] == null) {
         return AddressModel.fromJson(address);
       } else {
-        throw Exception('Cep not found');
+        throw InvalidZipException('CEP not found.');
       }
+    } else if (response.statusCode == 400) {
+      throw EmptyZipException('User did not type a CEP.');
     } else {
-      throw Exception('Cep not found 2');
+      throw Exception(
+          'Error when trying to access API. Status code: ${response.statusCode}');
     }
   }
 }
