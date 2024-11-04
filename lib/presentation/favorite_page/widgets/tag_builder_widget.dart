00954d01d300b65ab33b5780c997f489d710facf
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-11-03 22:51:40 -0300

    WIP missing adjustment of tag alignment

--- lib/presentation/favorite_page/widgets/tag_builder_widget.dart
+++ lib/presentation/favorite_page/widgets/tag_builder_widget.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zip_search/core/model/favorite_model.dart';
 import 'package:zip_search/presentation/favorite_page/cubit/favorite_cubit.dart';
-import 'package:zip_search/presentation/favorite_page/widgets/create_tag_widget.dart';
 
+import '../../../core/commons/app_strings.dart';
 import '../cubit/favorite_state.dart';
+import 'create_tag_widget.dart';
 
-class TagBuilderWidget extends StatelessWidget {
+class CardTagsWidget extends StatelessWidget {
   final FavoriteModel favoriteAddress;
 
-  const TagBuilderWidget({
+  const CardTagsWidget({
     super.key,
     required this.favoriteAddress,
   });
@@ -19,72 +20,100 @@ class TagBuilderWidget extends StatelessWidget {
     final favoriteCubit = context.read<FavoriteCubit>();
 
     return BlocBuilder<FavoriteCubit, FavoriteState>(
-      builder: (_, state) {
+      builder: (context, state) {
         switch (state) {
-          case LoadFavoriteZipState s:
-            return Wrap(
-              spacing: 6.0,
-              runSpacing: 8.0,
-              children: [
-                ...List.generate(
-                  favoriteAddress.tags.length,
-                  (index) => _TagWidget2(
-                    tagName: favoriteAddress.tags.elementAt(index),
-                    action: () {
-                      favoriteCubit.createTag(
-                        favoriteAddress: favoriteAddress,
-                        tag: favoriteAddress.tags.elementAt(index),
-                      );
-                    },
-                  ),
-                ),
-                Visibility(
-                  visible: favoriteAddress.tags.length < 5,
-                  child: _TagWidget2(
-                    action: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BlocProvider.value(
-                            value: favoriteCubit,
-                            child: CreateTagWidget(
-                              favoriteAddress: favoriteAddress,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                )
-              ],
+          case AddedTagZipState s:
+            return _TagBuilderWidget(
+              tagAmount: s.tags.length,
+              tags: s.tags,
+              favoriteCubit: favoriteCubit,
+              favoriteAddress: favoriteAddress,
             );
           default:
-            return Center(child: const Text('caiu no default'));
+            return _TagBuilderWidget(
+              tagAmount: favoriteAddress.tags.length,
+              tags: favoriteAddress.tags,
+              favoriteCubit: favoriteCubit,
+              favoriteAddress: favoriteAddress,
+            );
         }
       },
     );
   }
 }
 
-class _TagWidget2 extends StatelessWidget {
-  final String? tagName;
+class _TagBuilderWidget extends StatelessWidget {
+  final FavoriteModel favoriteAddress;
+  final FavoriteCubit favoriteCubit;
+  final int tagAmount;
+  final List<String> tags;
+
+  const _TagBuilderWidget({
+    required this.favoriteAddress,
+    required this.favoriteCubit,
+    required this.tagAmount,
+    required this.tags,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) => Wrap(
+        spacing: 6.0,
+        runSpacing: 8.0,
+        children: [
+          ...List.generate(
+            tagAmount,
+            (index) => _TagWidget(
+              tagName: tags.elementAt(index),
+              action: () {
+                favoriteCubit.createTag(
+                  favoriteAddress: favoriteAddress,
+                  tag: tags.elementAt(index),
+                );
+              },
+            ),
+          ),
+          Visibility(
+            visible: tags.length < 5,
+            child: TextButton.icon(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BlocProvider.value(
+                      value: favoriteCubit,
+                      child: CreateTagWidget(
+                        favoriteAddress: favoriteAddress,
+                      ),
+                    ),
+                  ),
+                );
+              },
+              icon: const Icon(
+                Icons.sell_outlined,
+                size: 16,
+              ),
+              label: const Text(AppStrings.addTagText),
+            ),
+          )
+        ],
+      );
+}
+
+class _TagWidget extends StatelessWidget {
+  final String tagName;
   final VoidCallback action;
 
-  const _TagWidget2({this.tagName, required this.action});
+  const _TagWidget({required this.tagName, required this.action});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: action,
       child: Container(
+        // height: 48,
         decoration: ShapeDecoration(
-          color: Theme.of(context).colorScheme.secondaryContainer,
-          shape: const StadiumBorder(
-            side: BorderSide(
-              color: Colors.blue,
-              width: 2,
-            ),
-          ),
+          color: Theme.of(context).colorScheme.primary,
+          shape: const StadiumBorder(),
         ),
         child: Padding(
           padding: const EdgeInsets.symmetric(
@@ -93,18 +122,19 @@ class _TagWidget2 extends StatelessWidget {
           ),
           child: Row(
             mainAxisSize: MainAxisSize.min,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              Icon(
+                Icons.close,
+                size: 16,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              const SizedBox(width: 4),
               Text(
-                tagName ?? 'Adicionar tag',
+                tagName,
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.onPrimary,
                 ),
-              ),
-              const SizedBox(width: 4),
-              Icon(
-                Icons.sell_outlined,
-                size: 16,
-                color: Theme.of(context).colorScheme.onPrimary,
               ),
             ],
           ),
