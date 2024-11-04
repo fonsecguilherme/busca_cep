b6a4e93b13e0499e5b108fb92931feb677374b0c
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-11-04 20:56:26 -0300

    feat: unfocus changes

--- lib/presentation/favorite_page/widgets/create_tag_widget.dart
+++ lib/presentation/favorite_page/widgets/create_tag_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zip_search/core/commons/messages.dart';
 import 'package:zip_search/core/model/favorite_model.dart';
 import 'package:zip_search/core/widgets/custom_elevated_button.dart';
+import 'package:zip_search/core/widgets/focus_widget.dart';
 import 'package:zip_search/presentation/favorite_page/cubit/favorite_cubit.dart';
 import 'package:zip_search/presentation/favorite_page/cubit/favorite_state.dart';
 
@@ -32,34 +33,36 @@ class _CreateTagWidgetState extends State<CreateTagWidget> {
             Navigator.pop(context);
           }
         },
-        child: Scaffold(
-          appBar: AppBar(),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 12.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  'Qual o nome da nova tag?',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                TextField(
-                  controller: tagInputController,
-                ),
-                const Spacer(),
-                CustomElevatedButton(
-                  icon: Icons.sell_outlined,
-                  title: 'Adicionar tag',
-                  onTap: () {
-                    favoriteCubit.createTag(
-                        favoriteAddress: widget.favoriteAddress,
-                        tag: tagInputController.text);
-                  },
-                ),
-              ],
+        child: FocusWidget(
+          child: Scaffold(
+            appBar: AppBar(),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 12.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Qual o nome da nova tag?',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  TextField(
+                    controller: tagInputController,
+                  ),
+                  const Spacer(),
+                  CustomElevatedButton(
+                    icon: Icons.sell_outlined,
+                    title: 'Adicionar tag',
+                    onTap: () {
+                      favoriteCubit.createTag(
+                          favoriteAddress: widget.favoriteAddress,
+                          tag: tagInputController.text);
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/presentation/search_page/widgets/initial_widget.dart
+++ lib/presentation/search_page/widgets/initial_widget.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zip_search/core/commons/analytics_events.dart';
 import 'package:zip_search/core/commons/app_strings.dart';
 import 'package:zip_search/core/widgets/custom_elevated_button.dart';
+import 'package:zip_search/core/widgets/focus_widget.dart';
 import 'package:zip_search/presentation/search_page/cubit/search_cubit.dart';
 import 'package:zip_search/presentation/search_page/cubit/search_state.dart';
 
@@ -31,68 +32,72 @@ class _InitialWidgetState extends State<InitialWidget> {
   }
 
   @override
-  Widget build(BuildContext context) => Column(
-        key: InitialWidget.initialWidgetKey,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            AppStrings.searchPageMessage,
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
-          const SizedBox(height: 12),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: TextField(
-              maxLength: 8,
-              controller: _zipController,
-              onEditingComplete: () {
-                widget.analytics.logEvent(
-                  name: SearchPageEvents.searchPageButton,
-                );
-                cubit.searchZip(zipCode: _zipController.text);
-                _zipController.clear();
-              },
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.all(16.0),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(
-                    color: Theme.of(context).colorScheme.outline,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(
-                    color: Theme.of(context).colorScheme.outline,
-                    width: 2.0,
-                  ),
-                ),
-                filled: true,
-                hintStyle: const TextStyle(color: Colors.grey),
-                hintText: AppStrings.textFieldText,
+  Widget build(BuildContext context) => FocusWidget(
+        child: Scaffold(
+          body: Column(
+            key: InitialWidget.initialWidgetKey,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AppStrings.searchPageMessage,
+                style: Theme.of(context).textTheme.titleSmall,
               ),
-            ),
-          ),
-          BlocBuilder<SearchCubit, SearchState>(
-            builder: (BuildContext context, SearchState state) {
-              if (state is LoadingSearchState) {
-                return const CircularProgressIndicator();
-              }
-              return CustomElevatedButton(
-                onTap: () {
-                  widget.analytics.logEvent(
-                    name: SearchPageEvents.searchPageButton,
+              const SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: TextField(
+                  maxLength: 8,
+                  controller: _zipController,
+                  onEditingComplete: () {
+                    widget.analytics.logEvent(
+                      name: SearchPageEvents.searchPageButton,
+                    );
+                    cubit.searchZip(zipCode: _zipController.text);
+                    _zipController.clear();
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(16.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.outline,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.outline,
+                        width: 2.0,
+                      ),
+                    ),
+                    filled: true,
+                    hintStyle: const TextStyle(color: Colors.grey),
+                    hintText: AppStrings.textFieldText,
+                  ),
+                ),
+              ),
+              BlocBuilder<SearchCubit, SearchState>(
+                builder: (BuildContext context, SearchState state) {
+                  if (state is LoadingSearchState) {
+                    return const CircularProgressIndicator();
+                  }
+                  return CustomElevatedButton(
+                    onTap: () {
+                      widget.analytics.logEvent(
+                        name: SearchPageEvents.searchPageButton,
+                      );
+                      cubit.searchZip(zipCode: _zipController.text);
+                      _zipController.clear();
+                    },
+                    icon: CupertinoIcons.search,
+                    title: AppStrings.searchPagebuttonText,
                   );
-                  cubit.searchZip(zipCode: _zipController.text);
-                  _zipController.clear();
                 },
-                icon: CupertinoIcons.search,
-                title: AppStrings.searchPagebuttonText,
-              );
-            },
+              ),
+            ],
           ),
-        ],
+        ),
       );
 
   @override
