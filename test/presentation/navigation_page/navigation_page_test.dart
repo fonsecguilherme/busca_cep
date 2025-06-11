b196635267c809c1a3df71eed8bff72ac2107ac4
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-06-10 21:38:24 -0300

    refactor: commented file

--- test/presentation/navigation_page/navigation_page_test.dart
+++ test/presentation/navigation_page/navigation_page_test.dart
@@ -1,15 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:zip_search/data/repositories/via_cep_repository_imp.dart';
-import 'package:zip_search/data/shared_services.dart';
-import 'package:zip_search/presentation/favorite_page/cubit/favorite_cubit.dart';
 import 'package:zip_search/presentation/navigation_page/cubit/navigation_cubit.dart';
 import 'package:zip_search/presentation/navigation_page/cubit/navigation_state.dart';
-import 'package:zip_search/presentation/navigation_page/navigation_page.dart';
-import 'package:zip_search/presentation/search_page/cubit/search_cubit.dart';
-import 'package:zip_search/presentation/theme/cubit/theme_cubit.dart';
 
 class MockNavigationCubit extends MockCubit<NavigationState>
     implements NavigationCubit {}
@@ -73,35 +64,35 @@ void main() {
   // );
 }
 
-final _sharedServices = SharedServices();
-final _repository = ViaCepRepositoryImp();
+// final _sharedServices = SharedServices();
+// final _repository = ViaCepRepositoryImp();
 
-Future<void> _createWidget(WidgetTester tester) async {
-  await tester.pumpWidget(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => NavigationCubit(),
-        ),
-        BlocProvider(
-          create: (context) => FavoriteCubit(
-            sharedServices: _sharedServices,
-          ),
-        ),
-        BlocProvider(
-          create: (context) => SearchCubit(
-            viaCepRepository: _repository,
-            sharedServices: _sharedServices,
-          ),
-        ),
-        BlocProvider(
-          create: (context) => ThemeCubit(),
-        )
-      ],
-      child: const MaterialApp(
-        home: NavigationPage(),
-      ),
-    ),
-  );
-  await tester.pump();
-}
+// Future<void> _createWidget(WidgetTester tester) async {
+//   await tester.pumpWidget(
+//     MultiBlocProvider(
+//       providers: [
+//         BlocProvider(
+//           create: (context) => NavigationCubit(),
+//         ),
+//         BlocProvider(
+//           create: (context) => FavoriteCubit(
+//             sharedServices: _sharedServices,
+//           ),
+//         ),
+//         BlocProvider(
+//           create: (context) => SearchCubit(
+//             viaCepRepository: _repository,
+//             sharedServices: _sharedServices,
+//           ),
+//         ),
+//         BlocProvider(
+//           create: (context) => ThemeCubit(),
+//         )
+//       ],
+//       child: const MaterialApp(
+//         home: NavigationPage(),
+//       ),
+//     ),
+//   );
+//   await tester.pump();
+// }
